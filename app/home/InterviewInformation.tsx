fa77f115d73953baa49923d670a508d039e6cf45
evakathov <evakathov@hotmail.com>
2024-11-25 10:17:06 +0100

    updateIsCompleted implemented

--- app/home/InterviewInformation.tsx
+++ app/home/InterviewInformation.tsx
@@ -1,9 +1,12 @@
+import ToDo from "@/components/ToDo";
+import React from "react";
 import { View, Text } from "react-native";
 
 export default function InterviewInformation() {
   return (
     <View>
       <Text>InterviewInformation Screen</Text>
+      <ToDo qualificationStepNumber={2}></ToDo>
     </View>
   );
 }
