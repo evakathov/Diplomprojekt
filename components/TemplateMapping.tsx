ee0d06b63f7fa3be78dd59a381296785fdd156cc
simonebt <100191509+simonebt@users.noreply.github.com>
2024-12-03 11:35:25 +0100

    VideoScreen

--- components/TemplateMapping.tsx
+++ components/TemplateMapping.tsx
@@ -1,14 +1,11 @@
-// Mapping for templateID to icon and route
 const templateIdMapping: Record<number, { icon: string; route: string }> = {
   352: { icon: "document", route: "./SurveyScreen" },
   353: { icon: "book", route: "./PreperationScreen" },
   354: { icon: "calendar", route: "./AppointmentScreen" },
-  402: { icon: "medical", route: "./MedicalExaminationScreen" },
+  402: { icon: "video", route: "/home/VideoScreen" }, // Absolut sti for VideoScreen
   403: { icon: "upload", route: "./UploadPictureScreen" },
   404: { icon: "personality", route: "./PersonalityTestScreen" },
   405: { icon: "info", route: "./FinalInterviewScreen" },
-  // Tilføj flere templateID mappings her...
 };
 
-// Eksporter mappingen som standardeksport
 export default templateIdMapping;
