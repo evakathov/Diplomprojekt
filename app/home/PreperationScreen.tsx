1fa6ffdf6746ad99236812149e4fecbc75296262
evakathov <evakathov@hotmail.com>
2025-01-12 19:52:30 +0100

    small things

--- app/home/PreperationScreen.tsx
+++ app/home/PreperationScreen.tsx
@@ -83,7 +83,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
-    elevation: 3,
+    //elevation: 3,
   },
   title: {
     fontSize: 24,
@@ -120,7 +120,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
-    //elevation: 3,
+    elevation: 3,
   },
   buttonPressed: {
     backgroundColor: "#C5D8B6", // Ændret farve når trykket
